5a0c833d13c1269c9c2cea84a2fcb27c127e0180
Hafizh arrasyiid syahbana <124231122+HafizhArrasyiidS@users.noreply.github.com>
2025-01-15 19:12:15 +0700

    page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,35 +8,42 @@ const socials = [
 	{
 		icon: <Linkedin size={20} />,
 		href: "https://www.linkedin.com/in/hafizh-arrasyiid-syahbana-3ab844291/",
-		label: <span style={{  fontSize: '14px' }}>Linkedin</span>,
-		handle: <span style={{  fontSize: '12px' }}>Hafizh Arrasyiid Syahbana</span>,
+		label: <span style={{ fontSize: '14px' }}>Linkedin</span>,
+		handle: <span style={{ fontSize: '12px' }}>Hafizh Arrasyiid Syahbana</span>,
 	},
 	{
 		icon: <Mail size={20} />,
 		href: "mailto:hafizhas14des@gmail.com?subject=Your%20Subject&body=Your%20Message",
 		label: <span style={{ fontSize: '14px' }}>Email</span>,
-		handle: <span style={{  fontSize: '12px' }}>hafizhas14des@gmail.com</span>,
+		handle: <span style={{ fontSize: '12px' }}>hafizhas14des@gmail.com</span>,
 	},
 	{
 		icon: <Github size={20} />,
 		href: "https://github.com/HafizhArrasyiidS",
-		label: <span style={{  fontSize: '14px' }}>Github</span>,
-		handle: <span style={{  fontSize: '12px' }}>HafizhArrasyiidS</span>,
+		label: <span style={{ fontSize: '14px' }}>Github</span>,
+		handle: <span style={{ fontSize: '12px' }}>HafizhArrasyiidS</span>,
 	},
 ];
 
 export default function Example() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+		<div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
-			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+			<div className="container flex flex-col items-center justify-center min-h-screen px-4 mx-auto mt-12">
+				<div className="mb-8">
+					<img
+						src="/images/2.jpg"
+						alt="Main Image"
+						className="w-32 h-32 rounded-full border-4 border-zinc-500"
+					/>
+				</div>
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto sm:grid-cols-2 lg:grid-cols-3 lg:gap-16">
 					{socials.map((s) => (
 						<Card>
 							<Link
 								href={s.href}
 								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+								className="p-4 relative flex flex-col items-center gap-4 duration-700 group sm:gap-6 sm:py-16 lg:py-24 lg:pb-48 sm:p-8 md:p-16"
 							>
 								<span
 									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
@@ -44,9 +51,9 @@ export default function Example() {
 								/>
 								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
 									{s.icon}
-								</span>{" "}
+								</span>
 								<div className="z-10 flex flex-col items-center">
-									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+									<span className="text-base font-medium duration-150 sm:text-lg lg:text-xl xl:text-3xl text-zinc-200 group-hover:text-white font-display">
 										{s.handle}
 									</span>
 									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
